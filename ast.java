ba2979b8004e51551c8eba625c1108ba074bf7e2
akeehbler <akeehbler@wisc.edu>
2021-11-07 15:46:43 -0600

    fff

--- ast.java
+++ ast.java
@@ -310,6 +310,7 @@ class VarDeclNode extends DeclNode {
         // if it is a struct check if its an invalid name
         else if (myType instanceof StructNode) {
             struct = ((StructNode)myType).getId();
+            System.out.println(struct.toString());
             sym = globalTab.lookupGlobal(struct.toString());
             if (sym == null) {
                 ErrMsg.fatal(myId.getLineNum(), myId.getCharNum(), "Undeclared identifier");
@@ -481,7 +482,6 @@ class StructDeclNode extends DeclNode {
             Sym symCheck = table.lookupLocal(myId.toString());
             SymTable structTable = new SymTable();
             myDeclList.analyze(structTable, table);
-            System.out.println(myId.toString());
             StructDefSym structDefSym = new StructDefSym(structTable, myId.toString());
             table.addDecl(myId.toString(), structDefSym);
             myId.addLink(structDefSym);
